86941d87f93ad17eb03e93d80cf78787dc10420d
LiranS <lirangamerz@gmail.com>
2023-12-10 16:43:33 +0200

    added github link in navbar

--- components/Header/Navbar.tsx
+++ components/Header/Navbar.tsx
@@ -1,10 +1,15 @@
 'use client'
 import React from 'react'
 import { ModeToggle } from './ModeToggle'
+import { FaGithub } from "react-icons/fa";
+import { Button } from '../ui/button';
 
 const Navbar = () => {
   return (
-    <div className='w-full p-2 flex justify-end'>
+    <div className='w-full p-2 flex justify-end gap-2'>
+        <Button>
+        <a href='https://github.com/iLiranS/tailwind-draftjs-shadcn-wysiwygEditor' target='_blank'><FaGithub/></a>
+        </Button>
         <ModeToggle/>
     </div>
   )
